2377afe7160d14a89b6783d3907b184f0398a307
enjcat <tlawltjq0110@naver.com>
2019-02-11 16:40:09 +0900

    인벤토리, 스테이지 1-4, 마을들 추가
    2019.02.11. 16:40

--- TimeTravler/Assets/Scripts/UI/InventoryUI/equipslot.cs
+++ TimeTravler/Assets/Scripts/UI/InventoryUI/equipslot.cs
@@ -6,6 +6,7 @@ using UnityEngine.EventSystems;
 
 public class equipslot : MonoBehaviour
 {
+    public ItemInfo info;
 
     public void OnPointerClick(BaseEventData Data)
     {
@@ -20,12 +21,37 @@ public class equipslot : MonoBehaviour
         {
             //오른쪽클릭 && 아이템 장착해제
             Debug.Log("뺀다");
+            if ((item.code.Substring(0, 3) == "141" || item.code.Substring(0, 3) == "142"))
+            {
+                Inventory.instance.equipment_Shield.GetComponent<Image>().sprite = Inventory.instance.defaultImage;
+                InventorySlot.sheild = true;
+            }
             Inventory.instance.Add(this.gameObject.GetComponent<Item_string>().code);
             this.GetComponent<Image>().sprite = Inventory.instance.defaultImage;
             item.code = null;
         }
 
 
+    }
+
+    public void OnTriggerEnter2D()
+    {
+        if (this.GetComponent<Item_string>().code != null)
+        {
+            info.SetItem(ItemManager.instance.GetItemInfo(int.Parse(this.GetComponent<Item_string>().code.Substring(0, 5))));
+            info.gameObject.SetActive(true);
+        }
+    
+        //info.gameObject.GetComponent<RectTransform>().position = 
+
+        Vector3 position = gameObject.GetComponent<RectTransform>().position;
+
+        info.gameObject.GetComponent<RectTransform>().position = new Vector3(position.x - 100, position.y - 40);
+    }
+
+    public void OnTriggerExit2D()
+    {
+        info.gameObject.SetActive(false);
     }
 
 }
