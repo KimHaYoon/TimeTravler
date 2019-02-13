93b6390a12a2befc390aedcf0900e7d68cbab1e7
AnDeoukKyi <44719109+AnDeoukKyi@users.noreply.github.com>
2019-02-13 18:49:06 +0900

    중보스킬완성

--- TimeTravler/Assets/Scripts/Monster/SkillWideArea.cs
+++ TimeTravler/Assets/Scripts/Monster/SkillWideArea.cs
@@ -5,43 +5,29 @@ using UnityEngine;
 
 public class SkillWideArea : MonoBehaviour
 {
+    public Player player;
+    public GameObject monster;
 
-    public int monsterNum;//몬스터 번호
-    public int effectNum;
-    private GameObject monsterBossOb;
-    private Monster monsterBossCs;
-    private SkillManager skillManager;
-    private RuntimeAnimatorController runtimeAnimatorController;
-    public Vector3 setPos;
-    public Vector3 widePos;
+    float x, y, Objx, Objy;
 
-    // Start is called before the first frame update
-    void Start()
+
+    private void Start()
     {
+        x = player.transform.position.x;
+        y = player.transform.position.y;
+        Objx = transform.position.x;
+        Objy = transform.position.y;
+        StartCoroutine(DamageCor());
+    }
 
-        monsterBossCs = GetComponent<Monster>();
-        skillManager = GetComponent<SkillManager>();
+    private IEnumerator DamageCor()
+    {
+        if (x > Objx - 1f && x < Objx + 1f && y > Objy - 10f && y < Objy + 10f)
+            player.GetComponent<Player>().Hurt(monster, false, 1.5f);//충돌 데미지
+        yield return new WaitForSeconds(0.2f);
         
-        if (effectNum == 0)
-        {
-            GetComponent<Animator>().runtimeAnimatorController = Resources.Load("Monster/AnimationControllers/" + Convert.ToString(monsterNum) + "/" + Convert.ToString(monsterNum) + "WideArea" + Convert.ToString(effectNum)) as RuntimeAnimatorController;
-            //애니매이션 컨트롤러만 변경
-            transform.position = widePos + setPos;
-        }
-        else
-        {
-            GetComponent<Animator>().runtimeAnimatorController = Resources.Load("Monster/AnimationControllers/" + Convert.ToString(monsterNum) + "/" + Convert.ToString(monsterNum) + "WideArea" + Convert.ToString(effectNum)) as RuntimeAnimatorController;
-            //애니매이션 컨트롤러만 변경
-            transform.position = widePos + setPos;
-        }
+        StartCoroutine(DamageCor());
     }
-
-    // Update is called once per frame
-    void Update()
-    {
-        transform.position = widePos + setPos;
-    }
-
     private void EndEffect()
     {
         Destroy(gameObject);
