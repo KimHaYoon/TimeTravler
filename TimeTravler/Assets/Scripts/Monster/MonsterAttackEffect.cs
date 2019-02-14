fe101c4de2e43c3370008d44edadce24eaf504d6
AnDeoukKyi <44719109+AnDeoukKyi@users.noreply.github.com>
2019-02-14 16:14:43 +0900

    몬스터 어텍이펙트 크기조절

--- TimeTravler/Assets/Scripts/Monster/MonsterAttackEffect.cs
+++ TimeTravler/Assets/Scripts/Monster/MonsterAttackEffect.cs
@@ -10,21 +10,24 @@ public class MonsterAttackEffect : MonoBehaviour
     public int monsterNum;//몬스터 번호
     public Vector3 setPos;//AttackEffectPosition 
     public int damage;
-    
+
+    public float xScale;
+    public float yScale;
 
     void Start()
     {
         player = GameObject.Find("Player");
+        transform.localScale = new Vector3(xScale, yScale, 1);
 
         GetComponent<Animator>().runtimeAnimatorController = Resources.Load("Monster/AnimationControllers/" + Convert.ToString(monsterNum) + "/" + Convert.ToString(monsterNum) + "AttackEffect") as RuntimeAnimatorController;//애니메니터 몬스터 번호에 맞춰서 설정
         
-        transform.position = player.transform.position + setPos;//AttackEffectPosition 
+        transform.position = player.transform.position + setPos + new Vector3(0, -0.5f, 0);//AttackEffectPosition 
 
     }
     
     void Update()
     {
-        transform.position = player.transform.position + setPos;//AttackEffectPosition 
+        transform.position = player.transform.position + setPos + new Vector3(0, -0.5f, 0); ;//AttackEffectPosition 
     }
     
 
